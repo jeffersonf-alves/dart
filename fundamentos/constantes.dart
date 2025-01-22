b1a095bc299bbea0b86dbed2651bcbf66a2fdd58
Jefferson Alves <61715554+jeffersonf-alves@users.noreply.github.com>
2025-01-21 23:31:44 -0300

    Fundamentos

--- fundamentos/constantes.dart
+++ fundamentos/constantes.dart
@@ -3,6 +3,15 @@ import 'dart:io';
 
 main() {
     print('Hello');
+    
+    final nome = 'Jefferson';
+
+    print(nome);
+    print(nome);
+    const nome2 = 'Pedro';
+    print(nome2);
+
+
 
     stdout.write('Informe o raio: ');
     final entradaDeUsuario = stdin.readLineSync();
---
+++ fundamentos/tipos_basicos.dart
@@ -0,0 +1,23 @@
+// Números (int e double)
+// String
+// Booleano
+
+main() {
+
+  int n1 = 3;
+  double n2 = -5.67.abs();
+  double n3 = double.parse("12.756");
+  num n4 = 6;
+
+  print(n2 + n3);
+
+
+  String s1 = "Bom dia";
+  String s2 = "Boa tarde";
+
+  bool chove = false;
+  bool nchove = true;
+
+  dynamic x = "Aceita qualquer tipo";
+  var y = "Outro texto";
+} 
