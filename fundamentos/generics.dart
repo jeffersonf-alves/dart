eb06b6664277f495317a60618a225cc85acf20ab
Jefferson Alves <61715554+jeffersonf-alves@users.noreply.github.com>
2025-01-25 22:33:04 -0300

    Fundamentos

---
+++ fundamentos/generics.dart
@@ -0,0 +1,12 @@
+
+
+void main() {
+  var listaCoisas = ['Banana', true, 123, 4.56];
+
+  Map<String, double> salarios = {
+    'Gerente': 19.000,
+    'Vendendor': 19345.5,
+    'Estagiario': 600.00
+  };
+
+}
