a1bb493f2be94ed1cefc0cc33de1517ee6c78490
Jefferson Alves <61715554+jeffersonf-alves@users.noreply.github.com>
2025-01-26 14:00:24 -0300

    Controles

---
+++ estrutura_de_controle/for_1.dart
@@ -0,0 +1,9 @@
+
+
+main() {
+
+
+  for(int a = 1; a < 10; a++ ) {
+    print(a);
+  }
+}
---
+++ estrutura_de_controle/if_else.dart
@@ -0,0 +1,12 @@
+import 'dart:math';
+
+
+main() {
+  var nota = Random().nextInt(11);
+
+  if(nota > 7) {
+    print('Aprovado!');
+  } else {
+    
+  }
+}
