7a59ea394b160d95d9095b7f925719ea35164100
Jefferson Alves <61715554+jeffersonf-alves@users.noreply.github.com>
2025-01-26 14:09:40 -0300

    Controles

--- estrutura_de_controle/for_1.dart
+++ estrutura_de_controle/for_1.dart
@@ -6,4 +6,14 @@ main() {
   for(int a = 1; a < 10; a++ ) {
     print(a);
   }
+
+  var notas = [8.9, 9.3, 7.8, 6.9, 9.1];
+
+  for(var a = 1; a < notas.length; a++) {
+    print('A note é $a');
+  }
+
+  for(var nota in notas) {
+    print('A note é $nota');
+  }
 }
